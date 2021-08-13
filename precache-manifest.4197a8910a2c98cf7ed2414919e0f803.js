dbf0ecb6e15379f76cba09f2baf80b8085829cca
Aashwin Tripathi <aashwinfeind@gmail.com>
2021-08-13 23:58:52 +0530

    Updates

---
+++ precache-manifest.4197a8910a2c98cf7ed2414919e0f803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6860d8d24c2e5b0d654e476357f80fa6",
+    "url": "/CodeCamp_5-Menu/index.html"
+  },
+  {
+    "revision": "0140a9123f04a26fe2c2",
+    "url": "/CodeCamp_5-Menu/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/CodeCamp_5-Menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodeCamp_5-Menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0140a9123f04a26fe2c2",
+    "url": "/CodeCamp_5-Menu/static/js/main.fac80350.chunk.js"
+  },
+  {
+    "revision": "e5839937a8c7f37b6430",
+    "url": "/CodeCamp_5-Menu/static/js/runtime-main.62bcc73e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/CodeCamp_5-Menu/precache-manifest.4197a8910a2c98cf7ed2414919e0f803.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CodeCamp_5-Menu/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
